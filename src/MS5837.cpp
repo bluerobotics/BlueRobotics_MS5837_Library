6da6a64c3eeb1f5f02f02e920ba4c07e0f94aa51
PaulZC <pc235603@gmail.com>
2021-01-27 06:34:49 +0000

    Change #defines to const uint8_t - this replaces part 2 of @NeilBaner PR #21

--- src/MS5837.cpp
+++ src/MS5837.cpp
@@ -1,12 +1,12 @@
 #include "MS5837.h"
 #include <Wire.h>
 
-#define MS5837_ADDR               0x76
-#define MS5837_RESET              0x1E
-#define MS5837_ADC_READ           0x00
-#define MS5837_PROM_READ          0xA0
-#define MS5837_CONVERT_D1_8192    0x4A
-#define MS5837_CONVERT_D2_8192    0x5A
+const uint8_t MS5837_ADDR = 0x76;
+const uint8_t MS5837_RESET = 0x1E;
+const uint8_t MS5837_ADC_READ = 0x00;
+const uint8_t MS5837_PROM_READ = 0xA0;
+const uint8_t MS5837_CONVERT_D1_8192 = 0x4A;
+const uint8_t MS5837_CONVERT_D2_8192 = 0x5A;
 
 const float MS5837::Pa = 100.0f;
 const float MS5837::bar = 0.001f;
